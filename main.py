0238ee62ed21be09f59152ebc5076d16e0ee315d
ronni2005 <richamishra2005d@gmail.com>
2026-07-07 15:30:33 +0530

    implemented TFIDF based similarity calculation

---
+++ main.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from src.similarity import calculate_similarity
+
+def read_file(file_path:Path)->str:
+    """Reads the content of a file and returns it as a string."""
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
+def interpret_score(score:float)->str:
+    #->str represents return type of function
+    """gives human readable interpretation"""
+    if score>=0.70:
+        return "best match"
+    elif score>=0.50:
+        return "good match"
+    elif score>=0.30:
+        return "poor match"
+    return "very poor match"
+
+def main():
+    data_path=Path("data")
+    resume_path=data_path/"resume.txt"
+    jd_path=data_path/"job_description.txt"
+    resume_text = read_file(resume_path)
+    jd_text = read_file(jd_path)
+    similarity = calculate_similarity(resume_text, jd_text)
+    print(f"Resume match score: {similarity*100:.2f}%")
+    print(f"Interpretation:{interpret_score(similarity)}")
+
+
+if __name__=="__main__":
+    main()
---
+++ src/similarity.py
@@ -0,0 +1,10 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def calculate_imilarity(resume_text:str ,jd_text:str)->float:
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform([resume_text, jd_text])
+
+    # Calculate cosine similarity
+    similarity_matrix = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])
+    return float(similarity_matrix[0][0])
